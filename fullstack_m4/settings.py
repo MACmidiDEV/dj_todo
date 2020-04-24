fabf02064a5ebc992e852ef0ff1a0d506caef3ce
MACmidiDev <macmidi.webdesigns@gmail.com>
2020-04-23 20:43:32 -0400

    heroku deploy

--- fullstack_m4/settings.py
+++ fullstack_m4/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'todo'
+    'todo',
 ]
 
 MIDDLEWARE = [
@@ -124,3 +124,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+
+
+
